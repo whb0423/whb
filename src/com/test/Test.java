73c4a4d2ec131c72431afee467c0932c629eebf4
chinaflame <chinaflame@china-flame.cn>
2017-05-27 17:01:37 +0800

    项目

---
+++ src/com/test/Test.java
@@ -0,0 +1,13 @@
+package com.test;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String a="100";
+		String b="3";
+		Integer c=Integer.parseInt(a)/Integer.parseInt(b);
+		System.out.println(c*2);
+	}
+
+}
